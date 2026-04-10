730bfd9bc6080e2cb21c1649072b11a139389877
ymz <321914292@qq.com>
2026-04-10 22:02:25 +0800

    集成 Spring Doc

---
+++ app/src/main/java/org/ymz/app/controller/HealthCheckController.java
@@ -0,0 +1,20 @@
+package org.ymz.app.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ymz.app.web.response.Response;
+
+/**
+ * 健康检查
+ * @author ymz
+ */
+@Hidden
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public Response<String> checkHealth() {
+        return Response.ok("ok");
+    }
+}
