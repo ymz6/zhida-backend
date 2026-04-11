cbd3ba51f6f6758ba85978362d47b2e4eb4b294f
ymz <321914292@qq.com>
2026-04-11 08:58:23 +0800

    集成 Spring AOP

--- app/src/main/java/org/ymz/app/Application.java
+++ app/src/main/java/org/ymz/app/Application.java
@@ -2,8 +2,10 @@ package org.ymz.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class Application {
 
     public static void main(String[] args) {
