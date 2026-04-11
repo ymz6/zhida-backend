c6f3f20c8eaa61fae4f00af3ae1200f197db6eb0
ymz <321914292@qq.com>
2026-04-11 08:39:23 +0800

    集成 Spring Validation
    全局异常处理器新增 Spring 参数校验异常处理分支

--- app/src/main/java/org/ymz/app/web/exception/GlobalExceptionHandler.java
+++ app/src/main/java/org/ymz/app/web/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package org.ymz.app.web.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.ymz.app.web.response.Response;
 import org.ymz.app.web.response.ResultCode;
 
@@ -21,6 +23,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public Response<Void> handleBusinessException(BusinessException e) {
         return Response.fail(e.getResultCode(), e.getMessage());
+    }
+
+    /**
+     * 处理 Spring 参数校验异常：
+     * BindException 处理对象参数校验
+     * HandlerMethodValidationException 处理方法参数校验
+     */
+    @ExceptionHandler({BindException.class, HandlerMethodValidationException.class})
+    public Response<Void> handleValidationException(Exception e) {
+        return Response.fail(ResultCode.INVALID_PARAM);
     }
 
     /**
